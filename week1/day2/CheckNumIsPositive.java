ddcef562bc12d6c51eded282f413fd28759628f9
aswathy-sivan-15 <aswathy.siva4314@gmail.com>
2026-03-09 23:42:39 +0530

    Add files via upload
    Week1 day 1 & day 2 assignments

---
+++ week1/day2/CheckNumIsPositive.java
@@ -0,0 +1,14 @@
+package week1.day2;
+public class CheckNumIsPositive {
+    public static void main(String[] args) {
+        int version =10;
+        if (version > 0) 
+        {
+            System.out.println(version + " "+ "is a positive number");    
+        }
+        else{
+            System.out.println(version + " "+ "is a negative number");
+        }
+    }
+
+}
